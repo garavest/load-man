22f9bd80d919664d974f412ec59042ece930f5b6
Seth Murphy <seth.murphy@garavest.com>
2023-10-11 19:26:16 -0500

    feat: added date helper to convert string to Date

--- src/lib/helpers/dateHelpers.ts
+++ src/lib/helpers/dateHelpers.ts
@@ -1,3 +1,10 @@
+function convertStringToDate(date: string): Date {
+  const dateParts = date.split(/\D/);
+  const [year, month, day] = dateParts;
+
+  return new Date(Number(year), Number(month) - 1, Number(day), 19, 0, 0, 0);
+}
+
 function getMonthName(month: number): string {
   return new Date(new Date().getFullYear(), month + 1, 0).toLocaleString("en-US", {
     month: "long"
@@ -8,4 +15,4 @@ function getDaysInMonth(month: number, year: number): number {
   return new Date(year, month + 1, 0).getDate();
 }
 
-export { getMonthName, getDaysInMonth };
+export { convertStringToDate, getMonthName, getDaysInMonth };
