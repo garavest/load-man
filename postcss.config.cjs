43046aff4885845ad47d142ecfab63ff02726eb9
Seth Murphy <seth.murphy@garavest.com>
2023-10-30 00:40:42 -0600

    chore: updated postcss config to remove tailwind nesting plugin

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,6 @@
 const autoprefixer = require("autoprefixer");
 const tailwindcss = require("tailwindcss");
-const nesting = require("tailwindcss/nesting");
 
 module.exports = {
-  plugins: [nesting, tailwindcss, autoprefixer]
+  plugins: [tailwindcss, autoprefixer]
 };
